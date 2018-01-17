2987b68644575e3cbd342b96331c145a84e697e9
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2018-01-17 15:36:14 +0000

    wasn't tracked

---
+++ interface_ini.py
@@ -0,0 +1,14 @@
+filePath    = './Data/temp/'
+archivePath = './Data/archive/'
+emailPath   = './emails/'
+letterPath  = './letters/'
+
+snFilePath      = filePath + 'sn.txt'
+idFilePath      = filePath + 'id.txt'
+configFilePath  = filePath + 'config.json'
+
+dbHost = '109.74.196.205'
+dbUser = 'master'
+dbName = 'Meter'
+dbPass = 'SSartori12;;'
+
