44c0bedb56427aeabec9e687b2da7df930c4fce1
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2019-03-01 12:38:50 +0000

    fixed quotes

--- insertEdata.py
+++ insertEdata.py
@@ -7,20 +7,28 @@ import meter_db as mdb      # for sql queries
 
 def uploadEdata(dataFileName):
     metaID = dataFileName.split('_')[0]
+    print "Meta ID is {}".format(metaID)
+    householdID = mdb.getHouseholdForMeta(metaID)
+    print "HOusehold ID is {}".format(householdID)
+
     sqlq = "LOAD DATA INFILE '/home/meter/data/" + dataFileName + "' INTO TABLE Electricity FIELDS TERMINATED BY ',' (dt,Watt) SET Meta_idMeta = " + str(metaID) + ";"
     mdb.executeSQL(sqlq)
     mdb.commit()
     updateDataQuality(metaID, 1)
+    print "MetaID {} is being processed".format(metaID)
 
     # update status
-    sqlq = "UPDATE Household \
-            SET `status`="6"\
-            WHERE `idHousehold` ='" + str(householdID) + "';"
+    sqlq = """UPDATE Household
+            SET `status`= '6'
+            WHERE `idHousehold` ='{}';
+            """.format(householdID)
     mdb.executeSQL(sqlq)
     mdb.commit()
+    print "Status for HH {} set to 6".format(householdID)
 
     # produce 1min and 10min data
-    os.system('cd Analysis/scripts/ && python /home/meter/Analysis/scripts/el_downsample.py')
+    os.system('python /home/meter/Analysis/scripts/el_downsample.py')
+    # os.system('cd Analysis/scripts/ && python /home/meter/Analysis/scripts/el_downsample.py')
 
 
 
@@ -34,3 +42,4 @@ def updateDataQuality(idMeta, Quality):
     mdb.commit()
 
 uploadEdata(sys.argv[1]) 
+print "python done"
